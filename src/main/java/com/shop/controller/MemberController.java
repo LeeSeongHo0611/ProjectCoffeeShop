a0a302d8c6fdf7ed32279e7c30c9c0e8934a151e
ho611611 <ho611611@hanmail.net>
2024-06-28 18:24:13 +0900

    메일 인증 토큰 진행 -> 카카오 네이버 구굴 로그인 은 커밋이 안되어 붙여 넣어야 됨

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -2,16 +2,17 @@ package com.shop.controller;
 
 import com.shop.dto.MemberFormDto;
 import com.shop.entity.Member;
+import com.shop.service.MailService;
 import com.shop.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/members")
 @Controller
@@ -19,6 +20,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
+    private final MailService mailService;
+
+    String confirm = "";
+    boolean confirmCheck = false;
 
     @GetMapping(value = "/new")
     public String memberForm(Model model){
@@ -32,6 +37,12 @@ public class MemberController {
         if(bindingResult.hasErrors()){
             return "member/memberForm";//다시 회원가입으로 돌려보닙니다.
         }
+
+        if(!confirmCheck){
+            model.addAttribute("errorMessage","이메일 인증을 하세요");
+            return "member/memberForm";
+        }
+
         try{
             //Member 객체 생성
             Member member = Member.createMember(memberFormDto, passwordEncoder);
@@ -54,4 +65,24 @@ public class MemberController {
         model.addAttribute("loginErrorMsg","아이디 또는 비밀번호를 확인해주세요");
         return "/member/memberLoginForm";
     }
+
+    //인증키 이메일 전송 post 맵핑
+    @PostMapping("/{email}/emailConfirm")
+    public @ResponseBody ResponseEntity emailConfirm(@PathVariable("email") String email)
+        throws Exception{
+        confirm = mailService.sendSimpleMessage(email);
+        return new ResponseEntity<String>("인증 메일을 보냈습니다.", HttpStatus.OK);
+    }
+
+
+    @PostMapping("/{code}/codeCheck")
+    public ResponseEntity<String> codeCheck(@PathVariable("code") String code) throws Exception{
+        if(confirm.equals(code)){
+            confirmCheck = true;
+            return new ResponseEntity<>("인증 성공하였습니다.", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("인증 코드를 올바르게 입력해 주세요.", HttpStatus.BAD_REQUEST);
+    }
+
+
 }
